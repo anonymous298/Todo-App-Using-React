04b6093ac51b064ea7651df783f025a671e0302a
anonymous298 <tackletalha@gmail.com>
2025-08-10 05:17:16 +0500

    good progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import Navbar from './components/Navbar.jsx'
+import ToDoList from './components/ToDoList.jsx'
 
 function App() {
 
   return (
     <>
-      
       <Navbar />
+      
+
+      <ToDoList />
     </>
   )
 }
